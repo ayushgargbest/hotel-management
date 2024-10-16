b4d7c6a890e3d26f61575477b7cbc0466b51c061
ayushgargbest <ayushgarg9986@gmail.com>
2024-10-17 00:33:17 +0530

    pata nabi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Screens/Login';  // Ensure the correct path for Login.jsx
-import Signup from './Screens/Signup';  // Ensure the correct path for Signup.jsx
-import Home from './Screens/Home';      // Ensure the correct path for Home.jsx
+import Login from './Screens/Login'; 
+import Signup from './Screens/Signup';  
+import Home from './Screens/Home';     
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />   {/* Default route to Login */}
+        <Route path="/" element={<Home />} />   
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
