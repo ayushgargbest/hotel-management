634ec5ce2b4b18f35b46a001f40d45d90699ce2c
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2024-10-19 16:44:47 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
+const cors  = require('cors')
+app.use(express.json())
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173/'
+}))
 
+app.get('/test',(req,res)=>{
+    res.send('test ok')
+});
+
+app.post('/register',(req,res)=>{
+    const{name,email,password}=req.body;
+    res.json({name,email,password})
+})
 
 app.listen(port);
